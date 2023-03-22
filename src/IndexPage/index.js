8d1e91572ca63c16ead76ba2da2aac3df6e1a84a
Prasann <prasannnarajendran@gmail.com>
2023-03-22 23:32:46 +0530

    Place

--- src/IndexPage/index.js
+++ src/IndexPage/index.js
@@ -1,32 +1,37 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const IndexPage = () => {
   const [places, setPlaces] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:4001/api/places").then((response) => {
       setPlaces(response.data);
       // console.log(response.data)
-    })
+    });
   }, []);
   return (
-    <div className="mt-8 grid  gap-x-1 gap-y-8 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+    <div className="mt-8 grid gap-x-2 gap-y-8 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
       {places.length > 0 &&
         places.map((place) => (
-          <Link to={'/place/'+ place._id}>
-            <div className="rounded-2xl">
+          <Link to={"/place/" + place._id} className='bg-gray-200 w-45'>
+            <div className="rounded-2xl ">
               {place.addedphotos?.[0] && (
                 <img
-                  src={"http://localhost:4001/uploads/" + place.addedphotos?.[0]}
+                  src={
+                    "http://localhost:4001/uploads/" + place.addedphotos?.[0]
+                  }
                   alt="loading.."
-                  className="rounded-2xl object-cover aspect-square mb-2  w-40"
+                  className="object-cover h-32 w-full"
                 />
               )}
             </div>
-
-              <h3 className="font-bold  text-sm truncate mb-3 leading-4">{place.title}</h3> 
-              <p className="leading-4 text-gray-500" >{place.address}</p>
-              <div className="mt-2">Rs. <span className="font-bold">{place.price}</span> /night</div>
+            <div className="mt-2">
+            <h3 className="font-bold truncate mt-2 text-sm mb-3 pl-2 ">
+              {place.title}
+            </h3>
+            <p className="leading-4 text-gray-500 mt-2 mb-2 pl-2 truncate">{place.address}</p>
+            <p className="mt-2 mb-4 pl-2">Rs. <span className="font-bold">{place.price}</span> /night</p>
+            </div>
           </Link>
         ))}
     </div>
--- src/PlacePage/index.js
+++ src/PlacePage/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./index.css";
 
-const PlacePage = () => {
+function PlacePage() {
   const { id } = useParams();
   const [place, setPlace] = useState([]);
+  const [showphotos, setShowPhotos] = useState(false);
   useEffect(() => {
     if (!id) {
       return;
@@ -14,31 +16,96 @@ const PlacePage = () => {
     });
   }, [id]);
 
-  if(!place) return;
+  if (!place) return;
+
+  if (showphotos) {
+    return (
+      <div className="bg-white absolute inset-0 min-h-screen ">
+        <div className="py-2 px-4 grid gap-2 bg-black text-white text-center overflow-hidden">
+          <div>
+            <h1>{place.title}</h1>
+          <button className=" fixed right-0 p-2 rounded-2xl justify-end flex gap-1" onClick={()=>setShowPhotos(false)}>
+          
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+          Close
+        </button>
+          </div>
+         
+          {place?.addedphotos?.map((photo) => {
+            return (
+              <img
+                src={"http://localhost:4001/uploads/" + photo}
+                alt="place"
+                className=""
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className="mt-8 bg-gray-100 -mx-8 px-8 py-8">
+    <>
       <h1 className="font-bold">{place.title}</h1>
-      <a className=' my-2 block font-semibold underline' rel='noreferrer' target='_blank' href={'https://maps.google.com/?q='+ place.address}>{place.address}</a>
-        <div className="grid gap-2 grid-cols-[2fr_1fr]">
-            <div>
-                {place.addedphotos?.[0] &&
-                <img src={'http://localhost:4001/uploads/'+ place.addedphotos[0]} alt='place'/>
-                }
-            </div>
-            <div>
-            {place.addedphotos?.[1] &&
-                <img src={'http://localhost:4001/uploads/'+ place.addedphotos[1]} alt='place'/>
-                }
-                 {place.addedphotos?.[2] &&
-                <img src={'http://localhost:4001/uploads/'+ place.addedphotos[2]} alt='place'/>
-                }
+      <a
+        className="my-2 block font-semibold underline"
+        rel="noreferrer"
+        target="_blank"
+        href={"https://maps.google.com/?q=" + place.address}
+      >
+        {place.address}
+      </a>
 
+      <div className="">
+        <div className="child">
+          {place.addedphotos?.[0] && (
+            <div className="relative p-2">
+              <img
+                src={"http://localhost:4001/uploads/" + place.addedphotos[0]}
+                alt="place"
+                className="one rounded"
+              />
+
+              <button
+                onClick={() => setShowPhotos(true)}
+                className="absolute right-30 bottom-2 text-white rounded-2xl p-2 text-xs bg-opacity-75 bg-primary m-2 flex gap-1"
+              >
+                More+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  />
+                </svg>
+              </button>
             </div>
+          )}
         </div>
-    
-    </div>
+      </div>
+    </>
   );
-};
+}
 
 export default PlacePage;
